dc0aec33323407c0c70f2ba4d3a313dcd0db4065
Jesho Benedict <jeshojlbenedict34@gmail.co>
2026-06-25 19:47:00 +0530

    portfolio

--- src/projects.jsx
+++ src/projects.jsx
@@ -2,7 +2,7 @@ function Projects(props) {
     return (
         <>
           <div className='projects-card'>
-             <img width="100%" height="200px" src={`${import.meta.env.BASE_URL}${props.image}`} alt="can't load image" />
+             <img height="180px" src={`${import.meta.env.BASE_URL}${props.image}`} alt="can't load image" />
                 <h3>{props.name}</h3>
                 <p>{props.description}</p>
           </div>
